a094b245d8fdd7dc046da6f09059703b6aca30a4
PERNON Etienne <etienne.pernon23@ccicampus.fr>
2024-02-23 12:09:14 +0100

    not FIXED => bd seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder
 {
@@ -10,8 +13,10 @@ class DatabaseSeeder
      * Seed the application's database.
      */
     function run() {
+        
+        User::Factory()->count(150)->create();
         // todo : this is not working but I don't know why
         // Add a default user with information (choice by me)
-        DB::run('INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES (1, "admin", "contact@pepina.com", "ThePepina67", "2021-09-01 00:00:00", "2021-09-01 00:00:00")');
+       // DB::run('INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES (1, "admin", "contact@pepina.com", "ThePepina67", "2021-09-01 00:00:00", "2021-09-01 00:00:00")');
     }
 }
